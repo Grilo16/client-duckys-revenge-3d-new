50be911a566101dc722289355d226cc50d673386
Thomas Britton <tbritton@dufrain.co.uk>
2023-06-29 03:30:51 +0100

    base name to process env

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
 
 
   return (
-    <Router basename="/duckys-revenge-3d/">
+    <Router basename={process.env.PUBLIC_URL}>                                                     
       <AppBaseComponent registeredPages={registeredPages}>
         <GameConsoleComponent>
         <Routes>
